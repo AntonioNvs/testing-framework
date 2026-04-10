9acc0cdbbf28a1728d3154fd8d27cf6fbb18f513
AntonioNvs <tonim2612@gmail.com>
2026-04-09 23:11:07 -0300

    6. Classes TestLoader e TestRunner

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from classes import TestCase, TestCaseTest, TestResult, TestSuite, TestSuiteTest
+from classes import TestCase, TestCaseTest, TestResult, TestSuite, TestSuiteTest, TestLoader, TestLoaderTest, TestRunner
     
 class MyTest(TestCase):
 
@@ -18,21 +18,8 @@ class MyTest(TestCase):
         print('test_c')
         
 
-result = TestResult()
-suite = TestSuite()
+loader = TestLoader()
+suite = loader.make_suite(TestLoaderTest)
 
-suite.add_test(TestCaseTest('test_result_success_run'))
-suite.add_test(TestCaseTest('test_result_failure_run'))
-suite.add_test(TestCaseTest('test_result_error_run'))
-suite.add_test(TestCaseTest('test_result_multiple_run'))
-suite.add_test(TestCaseTest('test_was_set_up'))
-suite.add_test(TestCaseTest('test_was_run'))
-suite.add_test(TestCaseTest('test_was_tear_down'))
-suite.add_test(TestCaseTest('test_template_method'))
-
-suite.add_test(TestSuiteTest('test_suite_size'))
-suite.add_test(TestSuiteTest('test_suite_success_run'))
-suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
-suite.run(result)
-print(result.summary())
+runner = TestRunner()
+runner.run(suite)
